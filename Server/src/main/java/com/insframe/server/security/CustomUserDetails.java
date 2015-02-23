2fc50e1f13e45d167863df3b9b7fad451f9a20f6
Piero Divasto <pdivasto@gmail.com>
2015-02-23 13:06:49 +0100

    Security implementation and *.insframe.server.account package was removed.

---
+++ Server/src/main/java/com/insframe/server/security/CustomUserDetails.java
@@ -0,0 +1,90 @@
+package com.insframe.server.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.insframe.server.model.User;
+
+@SuppressWarnings("serial")
+public class CustomUserDetails implements UserDetails {
+	
+	private User user;
+
+	boolean status = false;
+	
+	Collection<? extends GrantedAuthority> authorities = null;
+	
+	public CustomUserDetails(User user, BCryptPasswordEncoder encoder){
+		this.user = user;
+		authorities = new ArrayList<GrantedAuthority>();
+		this.setAuthentication(true);
+		String enc = encoder.encode(user.getPassword());
+		this.setPassword(enc);
+		Collection<UserRoles> roles = new ArrayList<UserRoles>();
+		UserRoles customRole = new UserRoles();
+		customRole.setAuthority(user.getRole());
+		roles.add(customRole);
+		this.setAuthorities(roles);
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.user.getUserName();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+	public void setAuthorities(Collection<? extends GrantedAuthority> roles) {
+		this.authorities = roles;
+	}
+
+	public void setAuthentication(boolean status) {
+		this.status = status;
+	}
+	
+	public void setPassword(String pass) {
+		this.user.setPassword(pass);
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+}
