7f164c8e5a237389965e168bee3e8b4f1f1cf3c6
Tobias Hoehmann <tobiashoehmann@googlemail.com>
2015-03-04 23:23:04 +0100

    Specific CORS filtering. #4

--- Server/src/main/java/com/insframe/server/support/service/SimpleCORSFilter.java
+++ Server/src/main/java/com/insframe/server/support/service/SimpleCORSFilter.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleCORSFilter implements Filter{
-	static final String ORIGIN = "Host";
+	static final String ORIGIN = "Origin";
 	
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
