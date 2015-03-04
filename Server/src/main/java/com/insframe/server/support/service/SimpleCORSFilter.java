3d49504e412d210bf1fb7a6d7d5f3137e9d5d4c2
Tobias Hoehmann <tobiashoehmann@googlemail.com>
2015-03-04 23:02:07 +0100

    Specific CORS filtering. #2

--- Server/src/main/java/com/insframe/server/support/service/SimpleCORSFilter.java
+++ Server/src/main/java/com/insframe/server/support/service/SimpleCORSFilter.java
@@ -17,7 +17,8 @@ public class SimpleCORSFilter implements Filter{
 	
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "localhost:8080,inspection-framework-client.herokuapp.com");
+		
+		response.setHeader("Access-Control-Allow-Origin", req.getLocalName());
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
